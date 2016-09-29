5e61976b87c48ecb22990d8daa3b8970e94c884b
Trevor D. Miller <trevordmiller@icloud.com>
2016-09-29 17:01:20 -0600

    Update to nova-colors 2

--- src/airline.js
+++ src/airline.js
@@ -1,5 +1,5 @@
+const colorValues = require('nova-colors').default
 const uiGroups = require('nova-colors').uiGroups
-const ansiGroups = require('nova-colors').ansiGroups
 
 /**
  * A note about the `airline#themes#generate_color_map` function
@@ -24,42 +24,42 @@ let s:modified = {
   \\ }
 
 " Normal mode
-let s:N1 = [ "${ansiGroups.bright.white}" , "${ansiGroups.bright.black}" , "" , ""  ]
-let s:N2 = [ "${ansiGroups.bright.white}" , "${uiGroups.gray3}" , "" , ""  ]
-let s:N3 = [ "${ansiGroups.normal.white}" , "${uiGroups.gray2}" , "" , ""  ]
+let s:N1 = [ "${uiGroups.gray6}" , "${uiGroups.gray4}" , "" , ""  ]
+let s:N2 = [ "${uiGroups.gray6}" , "${uiGroups.gray3}" , "" , ""  ]
+let s:N3 = [ "${uiGroups.foreground}" , "${uiGroups.gray2}" , "" , ""  ]
 let g:airline#themes#nova#palette.normal = airline#themes#generate_color_map(s:N1, s:N2, s:N3)
 let g:airline#themes#nova#palette.normal_modified = s:modified
 
 " Insert mode
-let s:I1 = [ "${uiGroups.gray1}" , "${ansiGroups.normal.yellow}", "" , ""  ]
+let s:I1 = [ "${uiGroups.gray1}" , "${colorValues.colors.yellow}", "" , ""  ]
 let s:I2 = s:N2
 let s:I3 = s:N3
 let g:airline#themes#nova#palette.insert = airline#themes#generate_color_map(s:I1, s:I2, s:I3)
 let g:airline#themes#nova#palette.insert_modified = s:modified
 
 " Visual mode
-let s:V1 = [ "${ansiGroups.bright.white}" , "${ansiGroups.bright.magenta}" , "" , "" ]
+let s:V1 = [ "${uiGroups.gray6}" , "${colorValues.colors.pink}" , "" , "" ]
 let s:V2 = s:N2
 let s:V3 = s:N3
 let g:airline#themes#nova#palette.visual = airline#themes#generate_color_map(s:V1, s:V2, s:V3)
 let g:airline#themes#nova#palette.visual_modified = s:modified
 
 " Replace mode
-let s:R1 = [ "${ansiGroups.bright.white}" , "${ansiGroups.normal.magenta}" , "" , "" ]
+let s:R1 = [ "${uiGroups.gray6}" , "${colorValues.colors.purple}" , "" , "" ]
 let s:R2 = s:N2
 let s:R3 = s:N3
 let g:airline#themes#nova#palette.replace = airline#themes#generate_color_map(s:R1, s:R2, s:R3)
 let g:airline#themes#nova#palette.replace_modified = s:modified
 
 " Inactive mode
-let s:IN1 = [ "${ansiGroups.normal.black}" , "${uiGroups.gray2}" , "" , "" ]
+let s:IN1 = [ "${uiGroups.background}" , "${uiGroups.gray2}" , "" , "" ]
 let s:IN2 = s:IN1
 let s:IN3 = s:IN1
 let g:airline#themes#nova#palette.inactive = airline#themes#generate_color_map(s:IN1, s:IN2, s:IN3)
 let g:airline#themes#nova#palette.inactive_modified = s:modified
 
 " Warning/Error Segment
-let s:AirlineError = [ "${uiGroups.gray6}" , "${ansiGroups.normal.red}" , "" , "" ]
+let s:AirlineError = [ "${uiGroups.gray6}" , "${uiGroups.userActionNeeded}" , "" , "" ]
 let g:airline#themes#nova#palette.normal.airline_error = s:AirlineError
 let g:airline#themes#nova#palette.insert.airline_error = s:AirlineError
 let g:airline#themes#nova#palette.visual.airline_error = s:AirlineError
@@ -69,7 +69,7 @@ let g:airline#themes#nova#palette.insert_modified.airline_error = s:AirlineError
 let g:airline#themes#nova#palette.visual_modified.airline_error = s:AirlineError
 let g:airline#themes#nova#palette.replace_modified.airline_error = s:AirlineError
 
-let s:AirlineWarning = [ "${uiGroups.gray6}" , "${ansiGroups.bright.red}" , "" , "" ]
+let s:AirlineWarning = [ "${uiGroups.gray6}" , "${colorValues.colors.orange}" , "" , "" ]
 let g:airline#themes#nova#palette.normal.airline_warning = s:AirlineWarning
 let g:airline#themes#nova#palette.insert.airline_warning = s:AirlineWarning
 let g:airline#themes#nova#palette.visual.airline_warning = s:AirlineWarning
@@ -87,7 +87,7 @@ let s:tabfill = airline#themes#get_highlight2(['Normal', 'bg'], ['Normal', 'bg']
 let g:airline#themes#nova#palette.tabline = {
   \\ 'airline_tab': s:N2,
   \\ 'airline_tabsel': s:N1,
-  \\ 'airline_tabtype': [ "${ansiGroups.normal.black}" , "${ansiGroups.normal.green}" , "" , "" ],
+  \\ 'airline_tabtype': [ "${uiGroups.background}" , "${colorValues.colors.green}" , "" , "" ],
   \\ 'airline_tabfill': s:tabfill,
   \\ 'airline_tabhid': s:IN2
   \\ }
